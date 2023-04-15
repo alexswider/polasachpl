09fac544ae7d5bfaf5157d0f10bc1d444d39ffde
Alex Swiderski <alex@fpo.studio>
2023-04-15 16:08:41 +0200

    ga fix maybe

--- polasach/src/main.js
+++ polasach/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import * as VueGoogleMaps from 'vue2-google-maps';
 import SocialSharing from 'vue-social-sharing';
 import './assets/styles.scss'; // For SCSS
+import VueAnalytics from "vue-analytics";
 
 Vue.use(SocialSharing);
 
@@ -11,6 +12,12 @@ Vue.use(VueGoogleMaps, {
     key: 'AIzaSyDdbvUUGfTHsbUB-sChAJfqQB2EiEXQV-Q', 
     libraries: 'maps',
   },
+});
+
+// Configure vue-analytics
+Vue.use(VueAnalytics, {
+  id: "UA-2621L863YQ", // Replace with your Google Analytics tracking ID
+  checkDuplicatedScript: true,
 });
 
 Vue.config.productionTip = false
